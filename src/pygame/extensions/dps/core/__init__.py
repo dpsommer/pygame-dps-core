0e60d3238f09cd47ce6b6e8b2ab721bcf9250558
Duncan Sommerville <duncan.sommerville@gmail.com>
2025-11-29 14:09:14 -0500

    Move sprite-related impl to separate extension package

--- src/pygame/extensions/dps/core/__init__.py
+++ src/pygame/extensions/dps/core/__init__.py
@@ -1,20 +1,18 @@
 from ._conf import init
-from .button import Button, ButtonOptions, SpriteOptions, TextOptions
 from .game import Game, GameSettings
 from .io import Configurable, Loadable
+from .keys import KeyBinding, is_key_pressed
 from .scenes import Scene, end_current_scene, get_active_scene, new_scene
 from .utils import debounce, normalize_path_str
 
 __all__ = [
     "Game",
     "GameSettings",
-    "Button",
-    "ButtonOptions",
-    "SpriteOptions",
-    "TextOptions",
     "Scene",
     "Configurable",
     "Loadable",
+    "KeyBinding",
+    "is_key_pressed",
     "end_current_scene",
     "get_active_scene",
     "new_scene",
