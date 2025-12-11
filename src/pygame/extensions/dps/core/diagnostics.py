b78f42b0f62d68c9f69fca804657581317099b12
Duncan Sommerville <duncan.sommerville@gmail.com>
2025-12-11 15:36:28 -0500

    Refactor diagnostics based on updated text functionality

--- src/pygame/extensions/dps/core/diagnostics.py
+++ src/pygame/extensions/dps/core/diagnostics.py
@@ -1,4 +1,5 @@
 import dataclasses
+import itertools
 from typing import Any, Dict, List, Type
 
 import pygame
@@ -8,7 +9,7 @@ from . import scenes, text
 
 @dataclasses.dataclass(frozen=True)
 class DiagnosticsSettings(text.TextOptions):
-    margin: int = 2
+    margins: text.Margins = dataclasses.field(default_factory=text.Margins)
 
 
 class Diagnostics(scenes.Scene):
@@ -19,8 +20,11 @@ class Diagnostics(scenes.Scene):
         super().__init__(screen)
         self.diagnostics: Dict[str, Any] = {}
         self._active_scene: scenes.Scene
-        self.opts = settings
-        self.margin = settings.margin
+        self.settings = settings
+        self.rect = self.screen.get_rect()
+        if self.settings.margins:
+            self.rect = self.settings.margins.apply(self.rect)
+        self.draw_group = pygame.sprite.LayeredDirty()
 
     def _on_enter(self):
         super()._on_enter()
@@ -28,31 +32,6 @@ class Diagnostics(scenes.Scene):
 
     def add(self, name: str, value: Any):
         self.diagnostics[name] = value
-
-    def draw(self) -> List[pygame.Rect]:
-        rects = self._active_scene.draw()
-
-        col_width = 0
-        _, window_height = pygame.display.get_window_size()
-        line_height = self.opts.font.get_linesize()
-
-        for i, diag in enumerate(self.diagnostics.items()):
-            name, val = diag
-            if type(val) is pygame.Rect:
-                val = val.center
-
-            max_in_col = int(
-                ((window_height - (self.margin * 2)) / (line_height + self.margin))
-            )
-            col, row = divmod(i, max_in_col)
-            x = (col * col_width) + self.margin
-            y = (row * line_height) + self.margin
-
-            img = text.draw_text(f"{name}: {val}", self.opts, self.screen, (x, y))
-            w, _ = img.get_size()
-            col_width = max(col_width, w)
-
-        return rects
 
     def handle_event(self, event: pygame.event.Event):
         if event.type == pygame.KEYDOWN:
@@ -63,6 +42,28 @@ class Diagnostics(scenes.Scene):
 
     def update(self, dt: float):
         self._active_scene.update(dt)
+        self.draw_group.empty()
+        self._create_diag_sprites()
+        self.draw_group.update()
+
+    def _create_diag_sprites(self):
+        text_cols = []
+
+        diag_strings = "\n".join([f"{k}: {v}" for k, v in self.diagnostics.items()])
+        lines_per_block = self.rect.h // self.settings.font.get_linesize()
+        text_cols.extend(itertools.batched(diag_strings, lines_per_block))
+
+        x, y = self.rect.topleft
+
+        for col in text_cols:
+            col_rect = pygame.Rect(x, y, self.rect.w - x, self.rect.h)
+            diags = text.multiline_text(col, self.settings, col_rect)
+            x += max(*diags, key=lambda d: d.rect.w).rect.w
+            self.draw_group.add(*diags)
+
+    def draw(self) -> List[pygame.Rect]:
+        rects = self._active_scene.draw()
+        return rects + self.draw_group.draw(self.screen)
 
     def dirty_all_sprites(self):
         self._active_scene.dirty_all_sprites()
