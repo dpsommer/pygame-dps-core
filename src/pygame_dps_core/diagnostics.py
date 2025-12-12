a2c910da1f991782ae06ad9a0dfc245af91b63ef
Duncan Sommerville <duncan.sommerville@gmail.com>
2025-12-12 11:26:34 -0500

    Refactor Diagnostics as Overlay subclass

--- src/pygame_dps_core/diagnostics.py
+++ src/pygame_dps_core/diagnostics.py
@@ -12,23 +12,18 @@ class DiagnosticsSettings(text.TextOptions):
     margins: text.Margins = dataclasses.field(default_factory=text.Margins)
 
 
-class Diagnostics(scenes.Scene):
+class Diagnostics(scenes.Overlay):
 
     settings_type: Type[DiagnosticsSettings] = DiagnosticsSettings
 
     def __init__(self, settings: DiagnosticsSettings, screen: pygame.Surface):
         super().__init__(screen)
         self.diagnostics: Dict[str, Any] = {}
-        self._active_scene: scenes.Scene
         self.settings = settings
         self.rect = self.screen.get_rect()
         if self.settings.margins:
             self.rect = self.settings.margins.apply(self.rect)
         self.draw_group = pygame.sprite.LayeredUpdates()
-
-    def _on_enter(self):
-        super()._on_enter()
-        self._active_scene = scenes.get_active_scene()
 
     def add(self, name: str, value: Any):
         self.diagnostics[name] = value
@@ -38,35 +33,29 @@ class Diagnostics(scenes.Scene):
             if event.key in (pygame.K_F12, pygame.K_ESCAPE):
                 scenes.end_current_scene()
                 return
-        self._active_scene.handle_event(event)
+        super().handle_event(event)
 
     def update(self, dt: float):
-        self._active_scene.update(dt)
+        super().update(dt)
         self.draw_group.empty()
         self._create_diag_sprites()
         self.draw_group.update()
 
     def _create_diag_sprites(self):
-        text_cols = []
-
         diag_strings = "\n".join([f"{k}: {v}" for k, v in self.diagnostics.items()])
         lines_per_block = self.rect.h // self.settings.font.get_linesize()
-        text_cols.extend(itertools.batched(diag_strings, lines_per_block))
+        text_cols = itertools.batched(diag_strings, lines_per_block)
 
         x, y = self.rect.topleft
 
         for col in text_cols:
             col_rect = pygame.Rect(x, y, self.rect.w - x, self.rect.h)
-            diags = text.multiline_text(col, self.settings, col_rect)
+            diags = text.multiline_text("\n".join(col), self.settings, col_rect)
             x += max(*diags, key=lambda d: d.rect.w).rect.w
             self.draw_group.add(*diags)
 
     def draw(self) -> List[pygame.Rect]:
-        rects = self._active_scene.draw()
-        return rects + self.draw_group.draw(self.screen)
+        return super().draw() + self.draw_group.draw(self.screen)
 
     def dirty_all_sprites(self):
-        self._active_scene.dirty_all_sprites()
-
-    def _reset(self):
-        pass  # no-op
+        super().dirty_all_sprites()
